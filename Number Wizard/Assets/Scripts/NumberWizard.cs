03ae228c7c0a413f226b2a6897c70b406402c7c0
Hasan Jafri <hasan.jafri7@gmail.com>
2018-01-14 01:55:49 -0500

    Number Wizard game guessing working upto 1000

---
+++ Number Wizard/Assets/Scripts/NumberWizard.cs
@@ -0,0 +1,48 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class NumberWizard : MonoBehaviour {
+
+	int max;
+	int min;
+	int guess;
+
+	// Use this for initialization
+	void Start () {
+		StartGame();
+	}
+
+	void StartGame () {
+
+		max = 1000;
+		min = 1;
+		guess = 500;
+
+		print ("Welcome to Number Wizard");
+		print ("Pick a number in your head!");
+		print ("Highest number you can pick is " + max + " and the lowest is " + min);
+		print ("Is the number higher or lower than " + guess + "? \nUp Arrow for higher, Down Arrow for lower, Return for equal");
+
+		max = max + 1;
+	}
+
+	// Update is called once per frame
+	void Update () {
+		if (Input.GetKeyDown(KeyCode.UpArrow)) {
+			min = guess;
+			NextGuess();
+		} else if (Input.GetKeyDown(KeyCode.DownArrow)) {
+			max = guess; 
+			NextGuess();
+		} else if (Input.GetKeyDown(KeyCode.Return)) {
+			print("I won!");
+			StartGame();
+		}
+	}
+
+	void NextGuess () {
+		guess = (max + min) / 2;
+		print ("Is the number higher or lower than " + guess + "? \nUp Arrow for higher, Down Arrow for lower, Return for equal");
+	}
+}
